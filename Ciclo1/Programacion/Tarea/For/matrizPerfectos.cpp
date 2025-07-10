1c178e4611e24f535bafebaec2237ab8b9ec4507
Envy_02 <frostgaming669@gmail.com>
2025-07-09 21:31:32 -0500

    Actualizado

--- Ciclo1/Programacion/Tarea/For/matrizPerfectos.cpp
+++ Ciclo1/Programacion/Tarea/For/matrizPerfectos.cpp
@@ -20,16 +20,26 @@ int main() {
         for(int j=0; j < 3; j++) {
             suma=0;
 
+            /**
+             * Por definición de números perfectos,
+             * son aquellos en los que la suma de sus
+             * divisores incluyendo el 1, da el mismo
+             * número. Ejemplo: 6 = 3 + 2 + 1
+             * 
+             * Entonces lo que hace el siguiente bucle
+             * será sumar todos los divisores del elemento
+             * en la matriz a partir del 1.
+             */
             for(int k=1; k < A[i][j]; k++) {
                 if(A[i][j] % k == 0) {
-                    suma += k;
+                    suma += k; // Si k es divisor, sumamos.
                 }
             }
 
             if(suma == A[i][j]) {
-                B[index] = A[i][j];
+                B[index] = A[i][j]; // Si es número perfecto, guardamos
 
-                index++;
+                index++; // Cambiamos de posición en el vector para almacenar más números perfectos
             }
 
             cout << A[i][j] << "\t";
@@ -39,7 +49,7 @@ int main() {
 
     cout << "El vector de perfectos es:" << endl;
     for(int i=0; i < index; i++) {
-        cout << B[i] << "\t";
+        cout << B[i] << "\t"; // Se usa index como guia para imprimir solo la cantidad de elementos guardados y no todo el vector
     }
 
     cout << endl;
